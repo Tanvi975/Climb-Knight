50bb40cbdb003c7a0e696a95665a2faece26e98d
Tanvi <tanvi87917@gmail.com>
2026-09-09 00:33:23 +0530

    feat:Add scrolling and infinite platform generation (#8)

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const knightImg = new Image();
 knightImg.src = 'assets/knight_spritesheet.png';
 const player = { x: 185, y: 500, width: 60, height: 60, vx: 0, vy: 0, speed: 5, gravity: 0.4, jumpStrength: -8, isGrounded: false, frameX: 0, maxFrame: 5, frameTimer: 0, frameInterval: 6, facingRight: true };
 
+const platforms = [
+    { x: 0, y: 500, width: 400, height: 8 },
+    { x: 0, y: 400, width: 400, height: 8 },
+    { x: 0, y: 300, width: 400, height: 8 },
+    { x: 0, y: 200, width: 400, height: 8 }
+];
+
+const platformGap = 60;
 const keys = {};
 
 window.addEventListener('keydown', function startMusic() {
@@ -44,6 +52,31 @@ function update() {
     if (player.x < 0) player.x = 0;
     if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
 
+    if (player.y < 200) {
+        player.y = 200;
+
+        for (let platform of platforms) {
+            platform.y += 2;
+        }
+    }
+
+    player.isGrounded = false;
+
+    for (let platform of platforms) {
+        if (
+            player.x < platform.x + platform.width &&
+            player.x + player.width > platform.x &&
+            player.y + player.height >= platform.y &&
+            player.y + player.height <= platform.y + platform.height + 10 &&
+            player.vy >= 0
+        ) {
+            player.y = platform.y - player.height;
+            player.vy = 0;
+            player.isGrounded = true;
+        }
+    }
+
+
     if (player.y + player.height >= 580) {
         player.y = 580 - player.height;
         player.vy = 0;
@@ -59,9 +92,20 @@ function update() {
             }
             player.frameTimer = 0;
         }
-    } else{
+    } else {
         player.frameX = 0;
         player.frameTimer = 0;
+    }
+    createNewPlatforms();
+}
+
+function createNewPlatforms() {
+    let highestPlatform = platforms[platforms.length - 1];
+
+    if (highestPlatform.y > -platformGap) {
+        let newY = highestPlatform.y - platformGap;
+
+        platforms.push({ x: 0, y: newY, width: 400, height: 8 });
     }
 }
 
@@ -69,6 +113,11 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#5C4033';
     ctx.fillRect(0, 580, canvas.width, 20);
+
+    ctx.fillStyle = '#5C4033';
+    for (let platform of platforms) {
+        ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+    }
 
     const frameWidth = knightImg.width / 6;
     const frameHeight = knightImg.height;
@@ -84,9 +133,8 @@ function draw() {
                 frameWidth, frameHeight,
                 0, 0,
                 player.width, player.height
-                );
-        }
-        else{
+            );
+        } else {
             ctx.drawImage(
                 knightImg,
                 player.frameX * frameWidth, 0,
